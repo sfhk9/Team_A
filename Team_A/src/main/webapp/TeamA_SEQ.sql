5f8dfe2dc1f26e35e010dd4f981ceb76adf682bd
tak0231 <woogelgel0231@gmail.com>
2021-11-22 22:12:40 +0900

    sql_sequence

---
+++ Team_A/src/main/webapp/TeamA_SEQ.sql
@@ -0,0 +1,44 @@
+create sequence cart_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence FAQ_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence FAQ_USER_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence goodsinfo_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence notice_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence orderCancel_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence orderlist_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence qna_seq
+start with 1
+increment by 1
+maxvalue 99999;
+
+create sequence review_seq
+start with 1
+increment by 1
+maxvalue 99999;
