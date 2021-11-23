4500f3dc91abcc9ba440d4582378641097b07166
tak0231 <woogelgel0231@gmail.com>
2021-11-23 21:16:28 +0900

    test2

--- Team_A/src/main/java/egov/web/NikeController.java
+++ Team_A/src/main/java/egov/web/NikeController.java
@@ -3,6 +3,7 @@ package egov.web;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import egov.service.FileService;
 import egov.service.NikeService;
@@ -12,4 +13,12 @@ public class NikeController {
 
 	@Resource(name = "nikeService")
 	NikeService nikeService;
+	
+	@RequestMapping("test2.do")
+    public String test2() throws Exception {
+
+        System.out.println("hi");
+
+        return "";
+    }
 }
