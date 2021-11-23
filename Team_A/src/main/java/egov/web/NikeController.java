5824e66d81677074be905a80a1eeeb9ea63b99eb
sfhk9 <sfhk9@121.160.155.210>
2021-11-23 21:17:21 +0900

    test1

--- Team_A/src/main/java/egov/web/NikeController.java
+++ Team_A/src/main/java/egov/web/NikeController.java
@@ -3,6 +3,8 @@ package egov.web;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import egov.service.FileService;
 import egov.service.NikeService;
@@ -12,4 +14,14 @@ public class NikeController {
 
 	@Resource(name = "nikeService")
 	NikeService nikeService;
+	
+	@RequestMapping("test.do")
+	public String test() throws Exception {
+		
+		System.out.println("hi");
+		
+		return "";
+	}
+	
+	
 }
